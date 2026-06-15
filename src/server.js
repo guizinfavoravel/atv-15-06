3c78524e0b029e66d664ed705f38e4f753cded76
GUILHERME MEDEIROS LEMOS <guilherme.lemos13@senaisp.edu.br>
2026-06-15 13:16:55 -0300

    atv 6

--- src/server.js
+++ src/server.js
@@ -3,19 +3,18 @@ import express from 'express';
 import routes from './routes/routes.js';
 import { initializeDatabase } from './configs/database.js';
 
-
 const app = express();
-
 
 app.use(express.json());
 app.use('/', routes);
 
+const PORT = process.env.PORT || 3000;
 
-initializeDatabase().then(() => {
-    const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-}).catch(err => {
+
+initializeDatabase().catch(err => {
     console.error("Erro ao inicializar o banco de dados:", err);
+    process.exit(1);
 });
