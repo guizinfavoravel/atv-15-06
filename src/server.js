4380d33affb93c3806bc42612bf6afc21fc13dc9
GUILHERME MEDEIROS LEMOS <guilherme.lemos13@senaisp.edu.br>
2026-06-15 13:01:41 -0300

    atv 2

--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
-import e from "express";
 import 'dotenv/config';
+import express from 'express';
 import routes from './routes/routes.js';
-import cors from "cors";
+import { initializeDatabase } from './configs/database.js';
 
-const app = e();
 
-app.use(cors());
-app.use(e.json());
+const app = express();
+
+
+app.use(express.json());
 app.use('/', routes);
 
-app.use(
-    "/uploads/images",
-    e.static("uploads/images")
-);
 
-const SERVER_PORT = process.env.SERVER_PORT;
-
-app.listen(SERVER_PORT, () => {
-    console.log(`Servidor rodando em: http://localhost:${SERVER_PORT}`);
-})
+initializeDatabase().then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+    });
+}).catch(err => {
+    console.error("Erro ao inicializar o banco de dados:", err);
+});
