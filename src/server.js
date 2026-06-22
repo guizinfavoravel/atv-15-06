bc69c5e4fda64bda396f18a397eb1798ec670268
GUILHERME MEDEIROS LEMOS <guilherme.lemos13@senaisp.edu.br>
2026-06-22 10:18:05 -0300

    correção de erros

--- src/server.js
+++ src/server.js
@@ -8,13 +8,26 @@ const app = express();
 app.use(express.json());
 app.use('/', routes);
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Servidor rodando na porta ${PORT}`);
+// });
 
-initializeDatabase().catch(err => {
+// initializeDatabase().catch(err => {
+//     console.error("Erro ao inicializar o banco de dados:", err);
+//     process.exit(1);
+// });
+
+app.use(
+    "/uploads/images",
+    express.static("uploads/images")
+)
+
+initializeDatabase().then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+    });
+}).catch(err => {
     console.error("Erro ao inicializar o banco de dados:", err);
-    process.exit(1);
 });
